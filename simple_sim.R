3d086dd8f563ae3146009cf9ccb0e8e808567dd2
Edmund Lo <edmund.lo@ru.nl>
2022-06-09 13:17:44 +0200

    Added number of ER strategies as a parameter Added scale range Added continuous vs ordinal (rounding) condition

--- simple_sim.R
+++ simple_sim.R
@@ -7,16 +7,18 @@ library(ppcor)
 # to do
 #======================
 
-# 1. add condition: number of ER strategies
-# 2. add condition: continuous vs ordinal (integer) scale
-# 3. add scale range (0-5, 0-10, 0-100 etc)
+# 1. Make number of ER strategies as a hyperparameter
 
 #======================
 # simulation setup
 #======================
 
-n <- 100 #number of observation (time points per "participant")
-simn <- 50 #number of simulations ("participant")
+n <- 10 #number of observation (time points per "participant")
+ERn <- 3 #number of ER strategies. (Single) Bray-curtis cannot handle ERn = 1 for now
+simn <- 2 #number of simulations ("participant")
+scalemin <- 0
+scalemax <- 100
+rounding <- TRUE # whether or not round scores to integers
 set.seed(1999)
 
 # curve to sample the starting value of ER strategy A[1]
@@ -45,42 +47,67 @@ funsim <- function(autocorrA,ratioA,independence){
   
   #---- simulating ER strategies
   
-  dfSim <- data.frame(a = 1:n, b = 1:n, c = 1:n)
+  dfSim <- data.frame(a = 1:n)
   dfSim$a[1] <- resample(1)
   for (i in 2:n){
     dfSim$a[i] <- dfSim$a[i-1]*autocorrA + (1-autocorrA)*resample(1)
   }
-  dfSim$b <- dfSim$a*ratioA + independence*(1-ratioA)*resample(n)
-  dfSim$c <- dfSim$a*ratioA + independence*(1-ratioA)*resample(n)
   
+
+  #Create other strategies
+  if(ERn>1){
+    for (i in 2:ERn){
+      dfSim[letters[i]] <- dfSim$a*ratioA + independence*(1-ratioA)*resample(n) 
+    }
+  }
+  
+  #limit the timeseries to min/max
+  dfSim[] <- apply(dfSim, 2, function(x) ifelse(x < scalemin , scalemin, x))
+  dfSim[] <- apply(dfSim, 2, function(x) ifelse(x > scalemax , scalemax, x))
+  
+  # rounding
+  if(rounding){
+    dfSim[] <- round(dfSim,0)
+  }
+                                 
   #---- ER Variability candidate indices
   
   dfNew <- dfSim
   
+  # Momentary SD
   dfNew$sd <- base::apply(dfSim,1,FUN = sd)
+  
+  # Momentary Euclidean distance
   
   dfNew$edist<-0
   for (i in 1:n){
     dfNew$edist[i] <- (dist(rbind(dfSim[i,],base::lapply(dfSim[,],FUN = mean))))
   }
   
-  dfNew$bray<-0
-  dfNew$bray.bal<-0
-  dfNew$bray.gra<-0
-  for (i in 1:n){
-    tempres <- bray.part(rbind(dfSim[i,],base::lapply(dfSim[,],FUN = mean)))
-    dfNew$bray[i] <- tempres$bray[1]
-    dfNew$bray.bal[i] <- tempres$bray.bal[1]
-    dfNew$bray.gra[i] <- tempres$bray.gra[1]
+  # Momentary Bray-Curtis dissimilarity
+  if (ERn > 1){
+      # there appears to be some limitation on the bray.part on handling 1 ER stgy only
+      # will throw 0 and warning message if there is only 1 ER stgy
+      for (i in 1:n){
+      tempres <- bray.part(rbind(dfSim[i,],base::lapply(dfSim[,],FUN = mean)))
+      dfNew$bray[i] <- tempres$bray[1]
+      dfNew$bray.bal[i] <- tempres$bray.bal[1]
+      dfNew$bray.gra[i] <- tempres$bray.gra[1]
+    }
+  }else{
+    dfNew$bray<-0
+    dfNew$bray.bal<-0
+    dfNew$bray.gra<-0
   }
-  
+
+  # Multi-site Bray-Curtis dissimilarity
   resBray<- beta.multi.abund(dfSim)
   
   
   #---- simOutput
   
   simOutput$em_autocorrA<-acf(dfSim$a, plot = FALSE)$acf[2] #empirical auto correlation
-  simOutput$em_cor <-mean(cor(dfSim)[1,2:length(cor(dfSim))^0.5]) #empirical correlation
+  simOutput$em_cor<- ifelse(ERn > 1,mean(cor(dfSim)[1,2:length(cor(dfSim))^0.5]),NA) #empirical correlation
   simOutput$mean_sd <- mean(dfNew$sd)
   simOutput$mean_edist <- mean(dfNew$edist)
   simOutput$mean_bray <- mean(dfNew$bray)
@@ -110,12 +137,14 @@ siminput <- expand.grid(hyperparameters, stringsAsFactors = FALSE)
 resOutput <- data.frame()
 for (i in 1:nrow(siminput)){
   resSim<-rdply(simn,funsim(siminput$autocorrA[i],siminput$ratioA[i],siminput$independence[i]))
-    if(length(resOutput)==0){
+  #resSim <- simOutput  
+  if(length(resOutput)==0){
       resOutput <- resSim
     }else{
       resOutput <- rbind.data.frame(resOutput,resSim)
     }
 }
+
 
 # ============
 # calculate the fit
